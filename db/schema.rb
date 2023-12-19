ce930fbe6dc8e46937ef7897c88bdb6e270106ab
Prithvi singh <prithvi.singh@1mg.com>
2023-12-19 20:10:53 +0530

    Modified code for deployment

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,25 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_12_19_090737) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_blog_posts_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
-    t.integer "blog_post_id", null: false
+    t.bigint "blog_post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["blog_post_id"], name: "index_comments_on_blog_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
